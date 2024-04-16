3420800c2e312daad876dd804f24a6b2f91cfd7a
juan12331 <juanxadez2@gmail.com>
2024-04-16 14:45:31 -0300

    terminando de estruturar html e começando o css do login.css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,13 +3,13 @@ import "./Login.css"
 
 function Login(){
     return(
-        <div>
+        <div class="container">
             <img src="../image/logo-senai-azul2.png" alt="logo senai" />
             <h1>Login</h1>
             <input type="text" placeholder="NOME" />
             <input type="password" placeholder="SENHA" />
             <button class ="entrar"> Entrar </button>
-            <button class="criar conta">Não tenho conta</button>
+            <button class="criar">Não tenho conta</button>
         </div>
     )
 }
