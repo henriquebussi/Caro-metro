e93b10b9ef0e0b4e0b7f239007182335ca24d111
henriquebussi <henriquebussidossantos@gmail.com>
2024-04-16 14:48:23 -0300

    teste

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,12 @@ import "./Login.css"
 
 function Login(){
     return(
-        <div>
-            <img src="../image/logo-senai-azul2.png" alt="logo senai" />
-            <h1>Login</h1>
+        <div className=" bg-gradient-to-t from-blue-400 to-slate-50">
+            <div className=" flex justify-between">
+                <h1>Login</h1>
+                <img src="../image/logo-senai-azul2.png" alt="logo senai" />
+            </div>
+
             <input type="text" placeholder="NOME" />
             <input type="password" placeholder="SENHA" />
             <button class ="entrar"> Entrar </button>
