52d366ff3b095d8406527e8abf35317f4dd34341
juan12331 <juanxadez2@gmail.com>
2024-04-16 14:38:35 -0300

    estruturando html login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,15 @@
 import Topbar from "../components/Topbar"
+import "./Login.css"
 
 function Login(){
     return(
         <div>
-            <Topbar/>
-            
+            <img src="../image/logo-senai-azul2.png" alt="logo senai" />
+            <h1>Login</h1>
+            <input type="text" placeholder="NOME" />
+            <input type="password" placeholder="SENHA" />
+            <button class ="entrar"> Entrar </button>
+            <button class="criar conta">Não tenho conta</button>
         </div>
     )
 }
