6a493f1570f0330e0d7b37dd00ab8f978240acbd
juan12331 <juanxadez2@gmail.com>
2024-04-16 15:05:09 -0300

    tentando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,15 @@
-import Topbar from "../components/Topbar"
+import React from 'react';
 import "./Login.css"
 
 function Login(){
+
     return(
         <div class="container">
             <img src="../image/logo-senai-azul2.png" alt="logo senai" />
             <h1>Login</h1>
-            <input type="text" placeholder="NOME" />
-            <input type="password" placeholder="SENHA" />
-            <button class ="entrar"> Entrar </button>
+            <input type="text" placeholder="NOME" id="nome"/>
+            <input type="password" placeholder="SENHA" id="senha"/>
+            <button class ="entrar" id="entrar"> <a href="/home">Entrar</a> </button>
             <button class="criar">Não tenho conta</button>
         </div>
     )
