e98006e8b8630dd4caa391735a0d6a3fa0b13474
juan12331 <juanxadez2@gmail.com>
2024-04-19 08:38:57 -0300

    atualização da navbar e criação Aluno.jsx/ Aluno.css

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,7 +3,7 @@ import './Topbar.css'
 function Topbar(){
     return(
         <div className=" bg-purple-500 text-white" class="navbar">
-        LISTA DE ALUNOS DO DESENVOLVIMENTO <img src="src\image\logo-senai-azul2.png" className='imagem' alt="logo senai" />
+        <span>LISTA DE ALUNOS DO DESENVOLVIMENTO</span> <img src="src\image\logo-senai-azul2.png" className='imagem' alt="logo senai" />
         </div>
     )
 }
