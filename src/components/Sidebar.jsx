fd05ec00435249fe5272504813f19547044ef059
juan12331 <juanxadez2@gmail.com>
2024-04-19 08:59:21 -0300

    create Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import './Sidebar.css'
+import Topbar from "./Topbar"
+
+function Sidebar(){
+    return(
+        <>
+        <nav>
+            <Topbar/>
+        </nav>
+        <div className="" class="Sidebar">
+            
+        </div>
+        </>
+    )
+}
+
+export default Sidebar
--- src/pages/Aluno.jsx
+++ src/pages/Aluno.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Aluno.css'
 
 function Aluno() {
   return (
