ce97d9926c9f3c13534603c00ba7fbe55cf88e14
Juan12331 <juanxadez2@gmail.com>
2024-04-21 20:17:42 -0300

    tive q tirar as rota tbm

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
-import Userpage from "./pages/Userpage";
 import Cadastro from "./pages/Cadastro";
 import Aluno from "./pages/Aluno"
 import Cadastro_aluno from "./pages/Cadastro_aluno";
@@ -11,7 +10,6 @@ function MainRoutes() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Homepage />} />
-      <Route path="/user" element={<Userpage />} />
       <Route path="/cadastro" element={<Cadastro/>}/>
       <Route path="/aluno" element={<Aluno/>}/>
       <Route path="/cadastro-aluno" element={<Cadastro_aluno/>}/>
