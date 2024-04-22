2199afd4438b8b7de2e6cb3346cb0954cfc104d2
Juan12331 <juanxadez2@gmail.com>
2024-04-21 21:57:02 -0300

    criando a pagina alunos (inimigo do css ksksksk)

--- src/pages/Aluno.jsx
+++ src/pages/Aluno.jsx
@@ -9,13 +9,21 @@ function Aluno() {
       <TopbarAluno/>
 
       <main>
-
+    <div className="centro">
       <div className="imageAluno">
-        <img src="" alt="" />
+        <img src="src\image\Max-Verstappen.png" alt="" class="maxPerfil"/>
       </div>
       <div className="info">
-        <div className="container">
+        <div className="container1">
+           <span className="nome informacao">Nome: Max Verstappen</span> {/*depois tera que mudar usando banco de dados */}
+            <span className="cpf informacao">CPF: 453.178.287-91</span>
+            <span className="email informacao">Email: Maxverstappin@Verstappen.com</span>
+            <span className="turma informacao">Turma: IDEV2 né vida</span>
+            <span className="responsavel informacao">Nome do responsável: Sophie-Marie Kumpen</span>
+            <span className="telefone informacao">Número: #1 lugar </span>
+            <span className="comentarios informacao">Comentários:enquanto tu ta indo ele ta vortano </span>
         </div>
+      </div>
       </div>
       </main>
     </div>
